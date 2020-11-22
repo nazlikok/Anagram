762c067bd83dc30ec00f4b6f82e35161582b0cb7
nazlikok <nazlikok@hotmail.com>
2020-11-22 11:25:56 +0300

    2.nd

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -17,25 +17,57 @@ public class Main {
         Arrays.sort(reductionsChar);
         int numReductions = 0;
 
-        if (reductionsChar.length == introducesChar.length) {
-            for (int i = 0; i < reductionsChar.length; i++) {
+        String deduction = "deduction";
+        char[] deductionChar = deduction.toCharArray();
+        Arrays.sort(deductionChar);
+        int numDeduction = 0;
+
+        if (deductionChar.length == introducesChar.length) {
+            for (int i = 0; i < introducesChar.length; i++) {
                 numIntroduces = introducesChar[i];
                 System.out.print(numIntroduces);
             }
             System.out.println();
             System.out.println(Arrays.toString(introducesChar));
-            for (int i = 0; i < reductionsChar.length; i++) {
-                numReductions = reductionsChar[i];
-                System.out.print(numReductions);
+            for (int i = 0; i < deductionChar.length; i++) {
+                numDeduction = deductionChar[i];
+                System.out.print(numDeduction);
             }
             System.out.println();
-            System.out.println(Arrays.toString(reductionsChar));
-
-            if (numIntroduces == numReductions) {
+            System.out.println(Arrays.toString(deductionChar));
+            if (numIntroduces == numDeduction) {
                 System.out.println("yes");
             } else {
                 System.out.println("no");
             }
+        } else
+        {
+            System.out.println(introduces + " and "+ deduction+ " have not even equal length");
         }
+
+        if (reductionsChar.length == introducesChar.length) {
+            for (int i = 0; i < introducesChar.length; i++) {
+                numIntroduces = introducesChar[i];
+                //System.out.print(numIntroduces);
+            }
+            //System.out.println();
+            //System.out.println(Arrays.toString(introducesChar));
+            for (int i = 0; i < reductionsChar.length; i++) {
+                numReductions = reductionsChar[i];
+                //System.out.print(numReductions);
+            }
+            //System.out.println();
+            //System.out.println(Arrays.toString(reductionsChar));
+
+            if (numIntroduces == numReductions) {
+                System.out.println("yes, " + introduces + " and " + reductions + " are anagram");
+            } else {
+                System.out.println("no");
+            }
+        } else
+        {
+            System.out.println("not even equal");
+        }
+
     }
 }

4561d33bf052e53eb4b716b8aae6859f9d7e944f
nazlikok <nazlikok@hotmail.com>
2020-11-22 11:15:30 +0300

    1.st

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,41 @@
 package com.company;
 
+import java.util.*;
+import java.lang.*;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        String introduces = "introduces";
+        char[] introducesChar = introduces.toCharArray();
+        Arrays.sort(introducesChar);
+        int numIntroduces = 0;
+
+        String reductions = "reductions";
+        char[] reductionsChar = reductions.toCharArray();
+        Arrays.sort(reductionsChar);
+        int numReductions = 0;
+
+        if (reductionsChar.length == introducesChar.length) {
+            for (int i = 0; i < reductionsChar.length; i++) {
+                numIntroduces = introducesChar[i];
+                System.out.print(numIntroduces);
+            }
+            System.out.println();
+            System.out.println(Arrays.toString(introducesChar));
+            for (int i = 0; i < reductionsChar.length; i++) {
+                numReductions = reductionsChar[i];
+                System.out.print(numReductions);
+            }
+            System.out.println();
+            System.out.println(Arrays.toString(reductionsChar));
+
+            if (numIntroduces == numReductions) {
+                System.out.println("yes");
+            } else {
+                System.out.println("no");
+            }
+        }
     }
 }
